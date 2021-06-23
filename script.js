437c556bf49c473d897af7a0fb29eb9dd5f78d45
MarianaVanucci <marianavanucciandrade@gmail.com>
2021-06-22 22:52:01 -0300

    inicial commit

---
+++ script.js
@@ -0,0 +1,22 @@
+console.log('Ei!');
+
+// Faça um documento html com dois botões e um input
+
+// Um botão vai avaliar se o input tem mais de 5 caracteres, 
+
+// se tiver vai colorir o fundo da página de vermelho, 
+
+// se não vai pintar o fundo da página de azul
+
+
+// O outro botão vai avaliar se o input tem texto. 
+
+// Se tiver vai limpar esse texto, deixar limpinho. 
+
+// E ao mesmo tempo vai avaliar se esse texto for grande 
+
+// e o fundo tiver vermelho ele vai pintar o fundo de verde, 
+
+// se esse texto for pequeno e o fundo tiver azul
+
+// ele vai pintar o fundo de amarelo
